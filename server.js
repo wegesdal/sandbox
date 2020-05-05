19ea148e9581647f7be52e80bf9e28328d484930
William Egesdal <wegesdal@Williams-MacBook.local>
2020-05-04 22:12:27 -1000

    cors

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://safe-tor-60738.herokuapp.com/',
+    optionsSuccessStatus: 200
+  }
+
+app.use(cors(corsOptions));
+app.options('*', cors());
+app.use('/', routes);
+
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
